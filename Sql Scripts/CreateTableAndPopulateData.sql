e3ae8bf6983a57c7939a886f6a2e2b04a0b4494f
Ilja <workgarbuzilja@gmail.com>
2024-06-06 07:40:10 +0200

    sql scripts

---
+++ Sql Scripts/CreateTableAndPopulateData.sql
@@ -0,0 +1,9 @@
+CREATE TABLE TestClasses (
+    Name VARCHAR(255)
+);
+
+
+INSERT INTO TestClasses (Name) VALUES ('Animal');
+INSERT INTO TestClasses (Name) VALUES ('Building');
+INSERT INTO TestClasses (Name) VALUES ('Application');
+INSERT INTO TestClasses (Name) VALUES ('Image');

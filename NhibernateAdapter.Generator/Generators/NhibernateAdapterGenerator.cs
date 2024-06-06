e7364311fc4d1c49d18ee1dff400765aa7e59ef1
Ilja <workgarbuzilja@gmail.com>
2024-06-06 12:34:31 +0200

    refactoring

---
+++ NhibernateAdapter.Generator/Generators/NhibernateAdapterGenerator.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+using System.Text;
+
+namespace NhibernateAdapter.Generator.Generators
+{
+    [Generator]
+    public class NhibernateAdapterGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterSourceOutput(
+                context.AnalyzerConfigOptionsProvider,
+                (sourceProductionContext, optionsProvider) =>
+                {
+                    //string connectionString = "Host=localhost;Port=5432;Database=pgtestdata;Username=postgres;Password=password";
+                    //var classNames = GetClassNamesFromDatabase(connectionString);
+
+                    //foreach (var className in classNames)
+                    //{
+                    //    var classSource = GenerateClassSource(className);
+                    //    sourceProductionContext.AddSource(
+                    //        $"{className}.cs",
+                    //        SourceText.From(classSource, Encoding.UTF8));
+
+                    //    var mappingSource = GenerateMappingSource(className);
+                    //    sourceProductionContext.AddSource(
+                    //        $"{className}Map.cs",
+                    //        SourceText.From(mappingSource, Encoding.UTF8));
+                    //}
+                });
+        }
+    }
+}
